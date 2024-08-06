5f429a1afe4fa386aeae6c1f709682e77b0e79bf
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2024-08-06 12:15:52 -0600

    Custom Checkbox + Color Adjustments

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,3 +1,6 @@
+import style from './Checkbox.module.sass'
+import CheckIcon from './svgs/CheckIcon';
+
 type Props = {
     text?: string;
 } & React.DetailedHTMLProps<
@@ -6,14 +9,17 @@ type Props = {
 >;
 
 const Checkbox = (props: Props) => {
-    const { text, ...checkBoxProps } = props;
+    const { text, className, ...checkBoxProps } = props;
 
     return (
-        <div className={props.className}>
-            <div className={`size-full flex flex-row gap-3`}>
-                <input {...checkBoxProps} type={`checkbox`} />
+        <div className={className}>
+            <label className={`size-full flex flex-row gap-3`}>
+                <div className={`grid size-5`}>
+                    <input {...checkBoxProps} type={`checkbox`} className={`${style.input} peer bg-cblue-300 dark:bg-cblue-800 checked:bg-cblue-500 checked:dark:bg-cblue-200`} />
+                    <CheckIcon className={`size-full object-contain col-start-1 row-start-1 dark:stroke-black hidden peer-checked:block`} />
+                </div>
                 {text}
-            </div>
+            </label>
         </div>
     );
 };
